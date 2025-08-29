cf7f5d9be1d2cbd736212db5a6ee7b21b5560ffe
1amKhush <khushvendras99@gmail.com>
2025-08-29 19:10:44 +0530

    Added static relay code in host.go

--- Internal/p2p/host.go
+++ Internal/p2p/host.go
@@ -8,17 +8,11 @@ import (
 	"os"
 	"time"
 
-	// "sync"
-	// "time"
-
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
-
-	//"github.com/libp2p/go-libp2p/core/routing"
-	// "github.com/multiformats/go-multiaddr"
 	"github.com/multiformats/go-multiaddr"
 	ma "github.com/multiformats/go-multiaddr"
 )
@@ -40,58 +34,33 @@ func NewHost(ctx context.Context, listenAddr string) (host.Host, *dht.IpfsDHT, e
 	var idht *dht.IpfsDHT
 	fmt.Println("debugging 11")
 
+	// Configure the static relay
+	relayAddr, err := multiaddr.NewMultiaddr("/dns4/libr-relay-y7ib.onrender.com/tcp/443/wss/p2p/12D3KooWAzCNSjMvp6PgQnixd9d3Wt2oz8qKYcdqmEh23ZcFyHV4")
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to parse relay address: %w", err)
+	}
+	relayInfo, err := peer.AddrInfoFromP2pAddr(relayAddr)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get AddrInfo from relay address: %w", err)
+	}
+
 	h, err := libp2p.New(
 		libp2p.Identity(priv),
 		libp2p.ListenAddrs(maddr),
+		// Use the static relay
+		libp2p.EnableAutoRelayWithStaticRelays([]peer.AddrInfo{*relayInfo}),
 	)
 	if err != nil {
 		panic(err)
 	}
 
 	// Create DHT with reference to host `h`
-	// idht, err := dht.New(ctx, h)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	h, err = libp2p.New(
-		libp2p.Identity(priv),
-		libp2p.ListenAddrs(maddr),
-		// libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
-		// 	return idht, nil
-		// }),
-		libp2p.EnableAutoRelayWithPeerSource(
-			func(ctx context.Context, num int) <-chan peer.AddrInfo {
-				ch := make(chan peer.AddrInfo, num)
-				go func() {
-					defer close(ch)
-
-					peers, err := idht.GetClosestPeers(ctx, h.ID().String()) // ✅ h is available now
-					if err != nil {
-						return
-					}
-
-					for i := range num {
-						select {
-						case <-ctx.Done():
-							return
-						default:
-							info := peer.AddrInfo{ID: peers[i]}
-							ch <- info
-						}
-					}
-				}()
-				return ch
-			},
-		),
-	)
+	idht, err = dht.New(ctx, h)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("debugging 14")
 
-	if err != nil {
-		return nil, nil, err
-	}
-
-	idht, err = dht.New(ctx, h)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -105,8 +74,6 @@ func NewHost(ctx context.Context, listenAddr string) (host.Host, *dht.IpfsDHT, e
 	log.Printf("Host listening on: %s/p2p/%s", h.Addrs()[0], h.ID())
 	return h, idht, nil
 }
-
-
 
 
 // Add these improvements to your main function and Client struct
@@ -171,67 +138,6 @@ func Bootstrap(ctx context.Context, h host.Host, d *dht.IpfsDHT) error {
 
 	return nil
 }
-
-// // You'll also need this improved bootstrap function as a method of Client
-// func Bootstrap(ctx context.Context) error {
-// 	// Default IPFS bootstrap nodes
-// 	bootstrapNodes := []string{
-// 		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-// 		"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJyrVwtbZg5gBMjTezGAJN",
-// 		"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zp7VCk8JpNUQLoUPF3HfrDAQGS52a8",
-// 		"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX89HWoNT4gEoNA7MzZqaGzyCu5w",
-// 		"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
-// 	}
-
-// 	fmt.Println("Connecting to bootstrap nodes...")
-// 	connected := 0
-
-// 	for _, addrStr := range bootstrapNodes {
-// 		addr, err := multiaddr.NewMultiaddr(addrStr)
-// 		if err != nil {
-// 			log.Printf("Invalid bootstrap address %s: %v", addrStr, err)
-// 			continue
-// 		}
-
-// 		pi, err := peer.AddrInfoFromP2pAddr(addr)
-// 		if err != nil {
-// 			log.Printf("Failed to parse bootstrap peer info %s: %v", addrStr, err)
-// 			continue
-// 		}
-
-// 		connectCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
-// 		if err := c.host.Connect(connectCtx, *pi); err != nil {
-// 			log.Printf("Failed to connect to bootstrap node %s: %v", pi.ID, err)
-// 		} else {
-// 			fmt.Printf("Connected to bootstrap node: %s\n", pi.ID)
-// 			connected++
-// 		}
-// 		cancel()
-// 	}
-
-// 	if connected == 0 {
-// 		return fmt.Errorf("failed to connect to any bootstrap nodes")
-// 	}
-
-// 	fmt.Printf("Successfully connected to %d bootstrap nodes\n", connected)
-
-// 	// Bootstrap the DHT
-// 	fmt.Println("Bootstrapping DHT...")
-// 	if err := c.dht.Bootstrap(ctx); err != nil {
-// 		return fmt.Errorf("failed to bootstrap DHT: %w", err)
-// 	}
-
-// 	// Wait for DHT to become ready
-// 	fmt.Println("Waiting for DHT to become ready...")
-// 	select {
-// 	case <-c.dht.RefreshRoutingTable():
-// 		fmt.Println("DHT routing table refreshed")
-// 	case <-time.After(60 * time.Second):
-// 		fmt.Println("DHT bootstrap timeout, continuing anyway...")
-// 	}
-
-// 	return nil
-// }
 
 func loadOrGeneratePrivateKey() (crypto.PrivKey, error) {
 	privBytes, err := os.ReadFile(privKeyFile)
